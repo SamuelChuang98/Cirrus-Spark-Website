869d4e59c956facdce65a411567b3ea62acd3d1d
Aminekabene <kabenea99@gmail.com>
2020-12-21 11:58:13 -0500

    r

--- BookNow_Page.php
+++ BookNow_Page.php
@@ -1,11 +1,18 @@
-<br> 
-<link rel="stylesheet" type="text/css" href="../css/BookNow_Page.css">
+<?php
+    $page_title = "Book Now";
+    include("includes/header.php");
+    ?>
+<link rel="stylesheet" type="text/css" href="css/BookNow_Page.css">
 <div class="container">
-  <img src="img/fbbanner.jpg" alt="Snow" style="width:100%;">
-  <div class="bottom-left">Bottom Left</div>
-  <div class="top-left">Top Left</div>
-  <div class="top-right">Top Right</div>
-  <div class="bottom-right">Bottom Right</div>
-  <div class="centered">Centered</div>
+  <img src="img/bluetech.jpg" alt="fb banner" style="width:100%;">
+  
+  <div class="centered"> <br>We would be more than HAPPY</br>
+      <br>to work with YOU!</br>
+  </div>
 </div>
-</br>
+
+<?php
+include("includes/footer.php");
+    ?>
+
+
