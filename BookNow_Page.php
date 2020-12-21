f2fdb9c11d50fa444bdeed4f1622604b26e372a7
Aminekabene <kabenea99@gmail.com>
2020-12-21 10:31:09 -0500

    css added

--- BookNow_Page.php
+++ BookNow_Page.php
@@ -1,2 +1,13 @@
+<br> 
+<img src="img/fbbanner.jpg" alt="Image not working :/" width="100%">
+<div class="centered">We would be more than happy</div>
+</br>
 
-<img src="img/fbbanner.jpg" alt="Image not working :/" width="1000" height="500">
+<div class="container">
+  <img src="img/fbbanner.jpg" alt="Snow" style="width:100%;">
+  <div class="bottom-left">Bottom Left</div>
+  <div class="top-left">Top Left</div>
+  <div class="top-right">Top Right</div>
+  <div class="bottom-right">Bottom Right</div>
+  <div class="centered">Centered</div>
+</div>
