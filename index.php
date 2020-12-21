39bbc195bc343881fe306def8977ba9426107665
Samuel Chuang <72810981+SamuelChuang98@users.noreply.github.com>
2020-12-21 12:04:38 -0500

    homepage part 1 & nav z-index

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
     $page_title = "Welcome to Cirrus Spark";
     include("includes/header.php");
+?>
 
+<section id="wrapper">
+    <section id="main-top">
+
+
+
+    </section>
+
+
+
+
+</section>
+
+<?php
     include("includes/footer.php");
 ?>
