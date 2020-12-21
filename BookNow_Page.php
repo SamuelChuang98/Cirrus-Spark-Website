d3b53d2900c34e2c632860215eda9bd900181563
Aminekabene <kabenea99@gmail.com>
2020-12-21 12:59:01 -0500

    r

--- BookNow_Page.php
+++ BookNow_Page.php
@@ -4,7 +4,7 @@
     ?>
 <link rel="stylesheet" type="text/css" href="css/BookNow_Page.css">
 <div class="container">
-  <img src="img/bluetech.jpg" alt="fb banner" style="width:100%;">
+  
   
   <div class="centered"> <br>We would be more than HAPPY</br>
       <br>to work with YOU!</br>
