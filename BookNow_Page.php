0a3d4c85da9da15074eda7aa3530b4fc63c08a7e
Aminekabene <kabenea99@gmail.com>
2020-12-21 10:43:05 -0500

    s

--- BookNow_Page.php
+++ BookNow_Page.php
@@ -1,13 +1,3 @@
 <br> 
-<img src="img/fbbanner.jpg" alt="Image not working :/" width="100%">
-<div class="centered">We would be more than happy</div>
+<link rel="stylesheet" type="text/css" href="../css/BookNow_Page.css">
 </br>
-
-<div class="container">
-  <img src="img/fbbanner.jpg" alt="Snow" style="width:100%;">
-  <div class="bottom-left">Bottom Left</div>
-  <div class="top-left">Top Left</div>
-  <div class="top-right">Top Right</div>
-  <div class="bottom-right">Bottom Right</div>
-  <div class="centered">Centered</div>
-</div>
