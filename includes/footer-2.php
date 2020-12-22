90b2882116bcabef8aa11c89ef5401dcf59fa3c6
Samuel Chuang <72810981+SamuelChuang98@users.noreply.github.com>
2020-12-22 18:14:52 -0500

    completed basic design of front page

---
+++ includes/footer-2.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="css/head-and-foot.css">
+
+<section id="beginProject">
+    <h2>LET'S WORK TOGETHER</h2>
+    <p>We would love to work with you!</p>
+    <button>BEGIN PROJECT</button>
+</section>
