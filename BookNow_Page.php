e73e965ed8dd1b5f47ad9fa1a16a409c7e6707c3
Aminekabene <kabenea99@gmail.com>
2020-12-22 21:25:37 -0500

    we re getting there !

--- BookNow_Page.php
+++ BookNow_Page.php
@@ -1,16 +1,35 @@
+<?php
+    $page_title = "Welcome to Cirrus Spark";
+    include("includes/header.php");
+?>
 
-
-<link rel="stylesheet" type="text/css" href="/css/BookNow_Page.css">
+<link rel="stylesheet" type="text/css" href="css/BookNow_Page.css">
 
 <section id="top">
-
-<div id ="centeredtext">
-    We would be more than <b>HAPPY</b></br>To work with <b>YOU!</b>
-</div>
-
 <div id="verticaltext">
     <h2>BOOK NOW</h2>
-</div2>
+</div>
+   <span> We would be more than <strong>HAPPY</strong></br>To work with <strong>YOU!</strong></span>
+</section>
+
+<section id="main">
+<img src="img/logo.png" alt="Cirrus Spark logo">
+
+ <section id="getintouch">
+     <h2><span>K</span><strong> GET IN TOUCH</strong></br><span id="span2">K</span> WITH US <strong>TODAY</strong></h2>
+
+     <p1>
+         Our <strong>step-by-step</strong> estimate process</br>
+         is built to deliver the service of</br>
+         <strong>YOUR</strong> requirements
+     </p1>
+
+     <list>
+         
+     </list>
+
+
+ </section>
 
 </section>
 
