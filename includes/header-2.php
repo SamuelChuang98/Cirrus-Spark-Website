d94232bf7d79f8b981b66aa880fa67a8b066e251
Samuel Chuang <72810981+SamuelChuang98@users.noreply.github.com>
2020-12-28 15:47:37 -0500

    Placed the header-2 as an include

---
+++ includes/header-2.php
@@ -0,0 +1,10 @@
+<section id="top">
+<div id="verticaltext">
+    <h2><?php echo $title?></h2>
+</div>
+    <div id="center-text">
+        <?php echo $first_line;?>
+        </br>
+        <?php echo $second_line;?>
+   </div>
+</section>
---
+++ portfolio.php
@@ -0,0 +1,20 @@
+<?php
+
+$page_title="Our Work | Cirrus Spark";
+$title="Our Work";
+$first_line="We would be more than <strong> HAPPY</strong>";
+$second_line="to work with <strong>YOU!</strong>";
+
+include("includes/header.php"); 
+include("includes/header-2.php")
+
+?>
+
+
+
+<?php 
+
+include("includes/footer-2.php"); 
+include("includes/footer.php"); 
+
+?>
