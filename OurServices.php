b95d1b02a1d5fb28373bdf0f995349bf2717455b
Aminekabene <kabenea99@gmail.com>
2020-12-30 17:59:03 -0500

    push

---
+++ OurServices.php
@@ -0,0 +1,92 @@
+<?php
+
+$page_title="Our Services";
+$title="Services";
+$first_line="We take care of the <strong>FANCY</strong> stuff";
+$second_line="so that <strong>YOU</strong> don't have to.";
+
+include("includes/header.php"); 
+include("includes/header-2.php");
+
+?>
+
+<link rel="stylesheet" type="text/css" href="css/OurServices.css">
+
+<section class= "Presentation_text">
+    <section class="left_Presentation_text">
+        <h3>PROFESSIONAL</br>WEBSITES</h3>
+        <p>We believe in bringing more user attention with good</br>
+        branding. This is why we spend extra time with the</br>
+        clients to discuss and plan out all the clear details of</br>
+        their business to finally create a fully developped</br> 
+        website that would reflect their brand.</p>
+
+        </br><input type="submit" Value="BEGIN PROJECT">
+    </section>
+    <section class="right_Presentation_text">
+        <div class="top_left">
+            <div></div>
+            <h4>SEO</h4>
+            
+            <p>
+            Lorem ipsum dolor sit amet,</br>
+            consectetur  adipiscing elit.</br>
+            In consequat enim finibus</br>
+            felis lobortis aliquet.
+            </p>
+        </div>
+
+        <div class="top_right">
+            <div></div>
+            <h4>CLEAN DESIGN</h4>
+            <p>
+            Lorem ipsum dolor sit amet,</br>
+            consectetur  adipiscing elit.</br>
+            In consequat enim finibus</br>
+            felis lobortis aliquet.
+            </p>
+        </div>
+
+        <div class="bottom_left">
+            <div></div>
+            <h4>MOBILE RESPONSIVE</h4>
+            <p>
+            Lorem ipsum dolor sit amet,</br>
+            consectetur  adipiscing elit.</br>
+            In consequat enim finibus</br>
+            felis lobortis aliquet.
+            </p>
+        </div>
+
+        <div class="bottom_right">
+            <div></div>
+            <h4>CUSTOM FUNCTIONALITIES</h4>
+            <p>
+            Lorem ipsum dolor sit amet,</br>
+            consectetur  adipiscing elit.</br>
+            In consequat enim finibus</br>
+            felis lobortis aliquet.
+            </p>
+        </div>
+
+    </section>
+</section>
+
+<section class="in_between">
+    <section class="additional_service">
+            <div></div>
+            <h4>Additional Service</br><strong>WEB HOSTING<strong></h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</br>
+            elit. In consequat enim finibus felis lobortis aliquet.</br>
+            Aenean gravida gravida metus sed dictum.</p>
+            
+    </section>
+</section>
+
+
+<?php 
+
+include("includes/footer-2.php"); 
+include("includes/footer.php"); 
+
+?>
