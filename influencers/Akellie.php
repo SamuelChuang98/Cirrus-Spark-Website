a1186280fb3c9d22e2b933974bd7a1cf27e5a9ab
Samuel Chuang <72810981+SamuelChuang98@users.noreply.github.com>
2021-05-10 11:30:43 -0400

    included header

--- influencers/Akellie.php
+++ influencers/Akellie.php
@@ -1,6 +1,6 @@
 <?php
     $page_title="AKellie";
-
+    include("includes/header.php");
     header('Location: https://www.facebook.com/boutiquevipcouture');
 
 ?>
--- influencers/Blankita.php
+++ influencers/Blankita.php
@@ -1,6 +1,6 @@
 <?php
     $page_title="Blankita";
-
+    include("includes/header.php");
     header('Location: https://www.facebook.com/boutiquevipcouture');
 
 ?>
