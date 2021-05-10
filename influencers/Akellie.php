ec93400c901757ee000054891617965b5d0f3509
Samuel Chuang <72810981+SamuelChuang98@users.noreply.github.com>
2021-05-10 13:49:20 -0400

    changes

--- influencers/Akellie.php
+++ influencers/Akellie.php
@@ -1,7 +1,25 @@
-<?php
-    $page_title="AKellie";
-    include("includes/header.php");
-    header('Location: https://www.facebook.com/boutiquevipcouture');
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195846661-4"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'UA-195846661-4');
+        </script>
+    </head>
+</html>
+
+<?php
+    $page_title="Akellie";
+    include("includes/header.php"); 
+    sleep(3);
+    header('Location: https://www.facebook.com/boutiquevipcouture');
 ?>
+
+<h1>
+    Please stand by...
+</h1>
 
--- influencers/Blankita.php
+++ influencers/Blankita.php
@@ -1,7 +1,25 @@
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195846661-4"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-195846661-4');
+        </script>
+    </head>
+</html>
+
 <?php
     $page_title="Blankita";
-    include("includes/header.php");
+    include("includes/header.php"); 
+    sleep(3);
     header('Location: https://www.facebook.com/boutiquevipcouture');
-
 ?>
+
+<h1>
+    Please stand by...
+</h1>
 
